3753c0863e18384ff57cfe12165dee66d951742d
repjas <52012627+repjas@users.noreply.github.com>
2022-11-16 19:33:29 +0100

    .

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import win32com.client as win32
 from tkinter import filedialog
 
+## CONFIGURATION
+left = 15
+width = 320
+top = 100
+position = {
+    'left': 15,
+    'width': 320,
+    'top': 100
+}
+
 ## CREAT EXCEL INSTANCE
 excel = win32.Dispatch('Excel.Application')
 excel.Visible = True
 print('Choose Excel file...')
 workbook_name = filedialog.askopenfilename()
-workbook = excel.Workbooks.Open(Filename=f'"{workbook_name}"')
+workbook = excel.Workbooks.Open(Filename=f'{workbook_name}')
 
 ## CREAT POWERPOINT INSTANCE
 ppt = win32.Dispatch('Powerpoint.Application')
@@ -14,6 +24,8 @@ ppt.Visible = True
 presentation_name = filedialog.askopenfilename()
 presentation = ppt.Presentations.Open(FileName=f'"{presentation_name}"')
 slides = presentation.Slides
+
+nmbr_of_shapes = {}
 
 ## LOOP THROUGH NAMED RANGES AND INSERT IN PRESENTATION
 for namedrange in workbook.Names:
@@ -25,29 +37,40 @@ for namedrange in workbook.Names:
         for i in name_split:
             if i.isdigit():
                 slide_nmbr = int(i)
+                nmbr_of_shapes[i] = 1
                 break
     excel.Range(rangename).CopyPicture()
     shape = slides[slide_nmbr-1].Shapes.Paste()
-    shape.left, shape.top = 25, 160
+    shape.left, shape.top, shape.width = left, top, width
     del slide_nmbr
+
+print(nmbr_of_shapes)
 
 ## LOOP THROUGH SHEETS, FIND GRAPHS AND INSERT IN PRESENTATION
 for sheet in workbook.Sheets:
     if 'RD' not in sheet.Name:
-        print(sheet.Name)
         name_split = sheet.Name.replace('-','').split()
         for i in name_split:
             if i.isdigit():
                 slide_nmbr = int(i)
+                if str(slide_nmbr) in nmbr_of_shapes:
+                    nmbr_of_shapes[i] += 1
+                else:
+                    nmbr_of_shapes[i] = 1
+                print(sheet.Name, nmbr_of_shapes[i])
                 break
         charts = sheet.ChartObjects()
         for chart in charts:
             chart.CopyPicture()
             shape = slides[slide_nmbr-1].Shapes.Paste()
-            shape.left, shape.top = 25, 160
+            if nmbr_of_shapes[i] > 1:
+                shape.left, shape.top, shape.width = (left+width+left), top, width
+            else:
+                shape.left, shape.top, shape.width = left, top, 640
         del slide_nmbr
 
-
+print(nmbr_of_shapes)
+        
 
 
 
